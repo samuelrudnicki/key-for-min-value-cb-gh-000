96f7767ed618bdbc35ad6afe89e9c908329df28a
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-08 11:49:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
-  name_hash.each do |value, key|
+  name_hash.each do |key, value|
     if min_value == nil || value <min_value
       min_key = key
       min_value = value
